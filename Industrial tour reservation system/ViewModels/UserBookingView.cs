ab11221185aeaf3bc660c5a70a9019094a495115
Omnia <omniagad25@gmail.com>
2022-05-16 23:50:40 +0200

    Omnia

---
+++ Industrial tour reservation system/ViewModels/UserBookingView.cs
@@ -0,0 +1,17 @@
+﻿using Industrial_tour_reservation_system.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Industrial_tour_reservation_system.ViewModels
+{
+    public class UserBookingView
+    {
+        public Visitor Visitor { get; set; }
+        public Package Package { get; set; }
+        public Booking View_Booking { get; set; }
+        public VisitorBooking VisitorBooking { get; set; }
+        public List<Package> Package_List { get; set; }
+    }
+}
