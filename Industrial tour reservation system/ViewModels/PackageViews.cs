233081cee8662a4e09fbafbecfce758bb2e8f771
MohamedAnter <primegamingxd4@gmail.com>
2022-05-16 20:50:29 +0200

    MainAdminController

---
+++ Industrial tour reservation system/ViewModels/PackageViews.cs
@@ -0,0 +1,16 @@
+﻿using Industrial_tour_reservation_system.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Industrial_tour_reservation_system.ViewModels
+{
+    public class PackageViews
+    {
+        public Package Package { get; set; }
+
+        public List<Subject> Subject_List { get; set; }
+        public List<Place> Place_List { get; set; }
+    }
+}
